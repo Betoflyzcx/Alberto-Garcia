0897d63b0433dd0d278ceec2a3e6e34fdf44ecef
Betoflyzcx <albertogarcia1268@yahoo.com>
2016-07-06 00:27:26 -0700

    Hello World //reupload

---
+++ Hwk/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Alberto Garcia
+ *
+ * Created on July 3, 2016, 9:26 AM
+ */
+
+  /* Purpose :  Create a Program that Outputs Hello World 
+ */
+
+//System Libraries
+#include <cstdlib>
+#include <iostream>
+using namespace std; //Namespace of the System Libraries
+
+
+//User Libraries
+
+
+
+//Global Constants
+
+
+
+//Function Prototypes
+
+
+//Execution Begins Here!
+int main(int argc, char** argv)
+/*Declare Variables
+
+ * 
+
+*/
+{
+    //Output
+    cout << "Hello World" << endl;
+    
+       //Exit Stage Right!
+    return 0;
+}
+
