ecdf9b17a18270f2c7fd127163d8c742f73c199e
alberto garcia <alberto garcia>
2016-07-17 21:07:01 -0700

    Signed-off-by: alberto garcia <alberto garcia>

--- Projects/Project-1_Game/main.cpp
+++ Projects/Project-1_Game/main.cpp
@@ -35,6 +35,31 @@ int main(int argc, char** argv) {
 	int y;
 	cin >> x;
 	cin >> y;
+	short Turn;  // Player 1's && Player2's Turn  
+	short P_or_A;
+	char OpnP1; // Player 1's Option
+	char OpnP2; // Player 2's Option
+
+				//Input Data
+	cout << "Enter the Length for the map\\t MAX FOR 'L' AND 'W' IS 20! \n";
+	cin >> x;
+	cout << "Now Enter the Width for the Map"\n;
+	cin >> y;
+	cout << "Are You playing against someone or do you wish to play against an 'ai'";
+	cout << "\n\n \t Enter 1 For Playing another 'Player' \t Enter 2 For playing an ai";
+	cin >> P_or_A;
+
+
+	cout << "Ready?\t Set \t Go !!\t\n";
+
+	switch (Turn) {
+	case1:
+
+		break;
+
+	case2:
+
+		break;
 
 
 	MapDraw(x, y); //Maps Length and Width
